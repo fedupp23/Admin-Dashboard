bd796083844e05fae989015cd2ae3b2198b35c21
krish <141345008+fedupp23@users.noreply.github.com>
2024-08-30 12:01:48 +0530

    minor save cnge

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import { BarChart2 } from "lucide-react"
+import { BarChart2, DollarSign, Settings, ShoppingBag, ShoppingCart, TrendingUp, User } from "lucide-react"
 
 const SIDEBAR_ITEMS=[
 {
@@ -6,6 +6,46 @@ const SIDEBAR_ITEMS=[
     icon:BarChart2,
     color:"#6366f1",
     path:"/",
+
+},
+{
+    name:"Products",
+    icon:ShoppingBag,
+    color:"#885CF6",
+    href:"/product",
+
+},
+{
+    name:"Users",
+    icon:User,
+    color:"#EC4899",
+    href:"/users",
+
+},
+{
+    name:"Sales",
+    icon:DollarSign,
+    color:"#10B981",
+    href:"/sales",
+
+},
+{
+    name:"Orders",
+    icon:ShoppingCart,
+    color:"#F59E0B",
+    href:"/orders",
+
+},{
+    name:"Analytics",
+    icon:TrendingUp,
+    color:"#3882F6",
+    href:"/analytics",
+
+},{
+    name:"Settings",
+    icon:Settings,
+    color:"#6EE787",
+    href:"/settings",
 
 }
 ]
