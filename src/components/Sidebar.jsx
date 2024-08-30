6f2fdfffbde60e28b5a6f073d9855bbdceaf3008
krish <141345008+fedupp23@users.noreply.github.com>
2024-08-30 15:03:21 +0530

    made sidebar icons

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,63 +1,86 @@
-import { BarChart2, DollarSign, Settings, ShoppingBag, ShoppingCart, TrendingUp, User } from "lucide-react"
+import { BarChart2, DollarSign, Menu, Settings, ShoppingBag, ShoppingCart, TrendingUp, User } from "lucide-react";
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 const SIDEBAR_ITEMS=[
 {
     name:"Overview",
-    icon:BarChart2,
+    href:"/",
+    icon: BarChart2,
     color:"#6366f1",
-    path:"/",
 
 },
 {
     name:"Products",
     icon:ShoppingBag,
-    color:"#885CF6",
     href:"/product",
+    color:"#885CF6",
 
 },
 {
     name:"Users",
     icon:User,
-    color:"#EC4899",
     href:"/users",
+    color:"#EC4899",
 
 },
 {
     name:"Sales",
     icon:DollarSign,
-    color:"#10B981",
     href:"/sales",
+    color:"#34D36F",
 
 },
 {
     name:"Orders",
     icon:ShoppingCart,
-    color:"#F59E0B",
     href:"/orders",
+    color:"mustard",
 
 },{
     name:"Analytics",
     icon:TrendingUp,
-    color:"#3882F6",
     href:"/analytics",
+    color:"#3882F6",
 
 },{
     name:"Settings",
     icon:Settings,
-    color:"#6EE787",
     href:"/settings",
-
+    color:"#F8312F",
 }
 ]
 
-
-
-
-
 const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
+  const [issideBarOpen,setIsSideBaropen]=useState(true);
+  return(
+    
+    <motion.div
+    className={`relative z-11 transition-all duration-300 ease-in-out flex-shrink-0 ${issideBarOpen ? 'w-64':'w:20'}`}
+    animate={{width:issideBarOpen?256:80}}
+    >
+        <div className="h-full bg-gray-800 bg-opacity-50 backdrop-blur-md p-4 flex flex-col border-r border-gray-700 ">
+           <motion.button
+           whileHover={{scale:1.1}}
+           whileTap={{scale:0.9}}
+           onClick={()=> setIsSideBaropen(!issideBarOpen)}
+           className='p-2 rounded-full hover:bg-gray-700 transition-colors max-w-fit'
+           >
+            <Menu size={24} />
+            </motion.button> 
+            <nav className="mt-8 flex-grow">
+                {SIDEBAR_ITEMS.map((item)=>(
+                    <Link key={item.href} to={item.href}>
+                    <motion.div className="flex items-center p-4 text-sm font-medium rounded-lg hover:bg-gray-700 transition-colors mb-2">
+                        <item.icon size={20} style={{color:item.color,minWidth:"20px"}}  />
+                    </motion.div>
+                </Link>
+                ))}
+            </nav>
+        </div>
+    </motion.div>
+    
   )
 }
-
 export default Sidebar
